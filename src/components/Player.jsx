3ccd11abc45959c17a110d8638d4e70213a539fa
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-08-29 02:22:28 +0530

    Added player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,61 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+  text-align: center;
+
+  & h2 {
+    color: #54a399;
+  }
+
+  & p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & input {
+    font: inherit;
+    border: 1px solid #54a399;
+    background-color: #192f2b;
+    border-radius: 4px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    padding: 0.27rem;
+    color: #d1f0ec;
+  }
+
+  & input:focus {
+    outline: none;
+  }
+
+  & button {
+    cursor: pointer;
+    background-color: #54a399;
+    border: 1px solid #54a399;
+    padding: 0.4rem 1rem;
+    color: #061e1a;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+    border-radius: 4px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+
+  & button:hover {
+    background-color: #3c8379;
+    border-color: #3c8379;
+  }
+`;
+
 function Player() {
   return (
-    <section id="player">
+    <Section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
         <input type="text" />
         <button>Set Name</button>
       </p>
-    </section>
+    </Section>
   );
 }
 
