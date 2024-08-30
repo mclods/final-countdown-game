a87f76fd6be19470f56de2533dfd9d947d19d431
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-08-31 02:43:04 +0530

    Added challenges component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css';
 import Player from './components/Player';
+import styled from 'styled-components';
+import TimerChallenge from './components/TimerChallenge';
+
+const Challenges = styled.div`
+  max-width: 50rem;
+  margin: 3rem auto;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+`;
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <Challenges>
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting Tough" targetTime={10} />
+        <TimerChallenge title="Pros Only" targetTime={15} />
+      </Challenges>
     </>
   );
 }
