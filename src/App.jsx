0ad849578a29d3571a2446f8ed2426fe18cba4d0
Sagnik Chakraborty <sagnikchk118@gmail.com>
2024-09-03 04:09:39 +0530

    Implemened toast show and hide

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <InstructionToast />
+      <InstructionToast gameActive={gameActive} />
       <Player />
       <Challenges>
         <TimerChallenge
